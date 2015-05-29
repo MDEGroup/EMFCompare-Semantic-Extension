7ebb40d9134e166b80cbe548ca5cc3a780c31c44
Lorenzo <addazi.lorenzo@gmail.com>
2015-05-30 01:27:45 +0200

    + Organization Information into POM.xml + Contributors Information into POM.xml + Match Engine Starting Skeleton

---
+++ it.univaq.disim.mdegroup.emfcompare.extension/src/it/univaq/disim/mdegroup/emfcompare/extension/match/SemanticMatchEngine.java
@@ -0,0 +1,15 @@
+package it.univaq.disim.mdegroup.emfcompare.extension.match;
+
+import org.eclipse.emf.compare.match.DefaultMatchEngine;
+import org.eclipse.emf.compare.match.IComparisonFactory;
+import org.eclipse.emf.compare.match.eobject.IEObjectMatcher;
+
+public class SemanticMatchEngine extends DefaultMatchEngine {
+
+	public SemanticMatchEngine(IEObjectMatcher matcher,
+			IComparisonFactory comparisonFactory) {
+		super(matcher, comparisonFactory);
+		// TODO Auto-generated constructor stub
+	}
+
+}
